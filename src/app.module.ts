1d52c002900006850e731fc35f8ba128b846de80
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-07-12 17:04:45 -0600

    Instalación de GraphQL

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
