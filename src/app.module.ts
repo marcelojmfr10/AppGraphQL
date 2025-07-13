f5a9b11e4d4a6e85da49344572f111a8e5e61c7e
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-07-12 18:47:29 -0600

    CRUD finalizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,34 @@
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { ItemsModule } from './items/items.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
     ItemsModule,
   ],
   controllers: [],
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CreateItemInput } from './dto/create-item.input';
-import { UpdateItemInput } from './dto/update-item.input';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateItemInput, UpdateItemInput } from './dto/inputs';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ItemsService {
-  create(createItemInput: CreateItemInput) {
-    return 'This action adds a new item';
+
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>
+  ) {
+
   }
 
-  findAll() {
-    return [];
+  async create(createItemInput: CreateItemInput): Promise<Item> {
+    const newItem = this.itemsRepository.create(createItemInput);
+    return await this.itemsRepository.save(newItem);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} item`;
+  async findAll(): Promise<Item[]> {
+    return this.itemsRepository.find();
   }
 
-  update(id: number, updateItemInput: UpdateItemInput) {
-    return `This action updates a #${id} item`;
+  async findOne(id: string): Promise<Item> {
+    const item = await this.itemsRepository.findOneBy({ id });
+    if (!item) throw new NotFoundException(`Item with id ${id} not found`);
+    return item;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} item`;
+  async update(id: string, updateItemInput: UpdateItemInput): Promise<Item> {
+    const item = await this.itemsRepository.preload(updateItemInput);
+
+    if(!item) throw new NotFoundException(`Item with id ${id} not found`);
+
+    return this.itemsRepository.save(item!);
+  }
+
+  async remove(id: string): Promise<Item> {
+    // todo: soft delete, integridad referencial
+    const item = await this.findOne(id);
+
+    await this.itemsRepository.remove(item);
+    return {
+      ...item, id
+    };
   }
 }
